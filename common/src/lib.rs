f4ee992b086ae30e9b7b3bd5644533c763a9f4e0
Nielesonimusso <saxerniels@hotmail.com>
2022-11-24 22:13:32 +0100

    Fixed mistake of incorrect year

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -4,7 +4,7 @@ use std::env::var;
 
 pub fn input_file_lines() -> Lines<BufReader<File>> {
     let package_name = var("CARGO_PKG_NAME").expect("invalid package");
-    let file = File::open(format!("2022/{package_name}/input.txt")).expect("no read");
+    let file = File::open(format!("2021/{package_name}/input.txt")).expect("no read");
     let reader = io::BufReader::new(file);
     reader.lines()
 }
